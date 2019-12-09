cbb8811546e15c172b4385d8239393a11cd080e1
放浪 軍師 <roamschemer@gmail.com>
2019-12-09 23:29:52 +0900

    場所移動前

--- ContentViewPrismReactiveProperty/ContentViewPrismReactiveProperty/ContentViews/ViewModels/MyViewViewModel.cs
+++ ContentViewPrismReactiveProperty/ContentViewPrismReactiveProperty/ContentViews/ViewModels/MyViewViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,15 @@ namespace ContentViewPrismReactiveProperty.ViewModels
 {
     public class MyViewViewModel : BindableBase
     {
-        public MyViewViewModel() 
+        public ReactiveCommand Button1 { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand Button2 { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand Button3 { get; private set; } = new ReactiveCommand();
+        public ReactiveProperty<string> Text { get; set; } = new ReactiveProperty<string>("Hello!ContentView!");
+        public MyViewViewModel()
         {
-
+            Button1.Subscribe(_ => Text.Value = "1");
+            Button2.Subscribe(_ => Text.Value = "2");
+            Button3.Subscribe(_ => Text.Value = "3");
         }
     }
 }
